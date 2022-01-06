b03ee9852a6d6c0acda8d54a523697b7544d8dc2
Michael Bremer <mbremer@inmediasp.de>
2022-01-06 11:18:27 +0100

    apollo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/arrivals">Arrivals</router-link> |
-        <router-link to="/departures">Departures</router-link>
-      </v-container>
-      <router-view/>
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-main>
+            <v-container>
+                <router-link to="/">Home</router-link> |
+                <router-link to="/arrivals">Arrivals</router-link> |
+                <router-link to="/departures">Departures</router-link> 
+                <!-- | <router-link to="/posts">Posts</router-link> -->
+            </v-container>
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
+    export default {
+        name: 'App',
 
-  data: () => ({
-    //
-  }),
-};
+        data: () => ({
+            //
+        }),
+    };
 </script>
 <style scoped>
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Departures from "../views/Departures";
 import Arrivals from "../views/Arrivals";
+// import Posts from "../views/Posts";
 
 Vue.use(VueRouter)
 
@@ -21,11 +22,17 @@ const routes = [
     path: '/arrivals',
     name: 'Arrivals',
     component: Arrivals
-  }
+  },
+  // {
+  //   path: '/posts',
+  //   name: 'Posts',
+  //   component: Posts
+  // }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
