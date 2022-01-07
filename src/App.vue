8e4a0e86909971b82fb22ee64d9f5905e234e70f
Michael Bremer <mbremer@inmediasp.de>
2022-01-07 13:37:20 +0100

    logout hinzugefügt

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
                 <v-icon>mdi-face-man</v-icon>
             </v-btn>
 
-            <v-btn v-if="authStatus" icon to="/logout">
+            <v-btn v-if="authStatus" icon @click="logOut()">
                 <v-icon>mdi-lock-open</v-icon>
             </v-btn>
 
